04e182a67f11b57fa4cbecb378e8fd47685eaa99
JenBrainnet <benebrak01@gmail.com>
2024-07-17 11:18:51 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,36 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean Code", 464);
+        Book effectiveJava = new Book("Effective Java", 416);
+        Book grokkingAlgorithms = new Book("Grokking Algorithms", 320);
+        Book designPatterns = new Book("Design Patterns", 669);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = effectiveJava;
+        books[2] = grokkingAlgorithms;
+        books[3] = designPatterns;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " " + book.getPages());
+        }
+        System.out.println("Replace Clean Code to Design Patterns");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " " + book.getPages());
+        }
+        System.out.println("Shown only books with name Clean Code");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName().equals("Clean Code")) {
+                System.out.println(book.getName() + " " + book.getPages());
+            }
+        }
+    }
+
+}
