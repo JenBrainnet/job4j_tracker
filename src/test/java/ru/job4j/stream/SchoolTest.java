7a270d2309918bd05c22dccdc9f1ebe20b55115a
JenBrainnet <benebrak01@gmail.com>
2025-02-08 15:04:03 +0200

    5. Фильтрация учеников [#110058]

---
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -0,0 +1,74 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SchoolTest {
+
+    @Test
+    void whenCollectClassA() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(40, "Surname4"),
+                new Student(50, "Surname5"),
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9")
+        );
+        School school = new School();
+        Predicate<Student> predicate =
+                student -> student.getScore() >= 70 && student.getScore() <= 100;
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = Arrays.asList(
+                new Student(70, "Surname7"),
+                new Student(90, "Surname9")
+        );
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCollectClassB() {
+        List<Student> students = List.of(
+                new Student(20, "Surname2"),
+                new Student(30, "Surname3"),
+                new Student(50, "Surname5"),
+                new Student(60, "Surname6"),
+                new Student(80, "Surname8")
+        );
+        School school = new School();
+        Predicate<Student> predicate =
+                student -> student.getScore() >= 50 && student.getScore() < 70;
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = Arrays.asList(
+                new Student(50, "Surname5"),
+                new Student(60, "Surname6")
+        );
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenCollectClassV() {
+        List<Student> students = List.of(
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(40, "Surname4"),
+                new Student(60, "Surname6"),
+                new Student(90, "Surname9")
+        );
+        School school = new School();
+        Predicate<Student> predicate =
+                student -> student.getScore() > 0 && student.getScore() < 50;
+        List<Student> result = school.collect(students, predicate);
+        List<Student> expected = Arrays.asList(
+                new Student(10, "Surname1"),
+                new Student(30, "Surname3"),
+                new Student(40, "Surname4")
+        );
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
