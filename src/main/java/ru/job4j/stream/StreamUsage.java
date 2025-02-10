70ee44a87fb7f57ecf716e4feb2ab16c1b84a82b
JenBrainnet <benebrak01@gmail.com>
2025-02-10 17:07:26 +0200

    10. Преобразование матрицы чисел в список чисел. FlatMap. [#110228]

--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -76,6 +76,15 @@ public class StreamUsage {
                         )
                 )
         );
+        List<List<Integer>> matrix = List.of(
+                List.of(1, 2),
+                List.of(3, 4)
+        );
+        System.out.println(
+                matrix.stream()
+                        .flatMap(List::stream)
+                        .toList()
+        );
     }
 
 }
---
+++ src/test/java/ru/job4j/stream/MatrixToListTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MatrixToListTest {
+
+    @Test
+    void whenInputSquareMatrix() {
+        Integer[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        List<Integer> result = MatrixToList.convert(matrix);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenInputNoSquareMatrix() {
+        Integer[][] matrix = {
+                {1},
+                {2, 3},
+                {4, 5, 6}
+        };
+        List<Integer> result = MatrixToList.convert(matrix);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
