949184e08179aea67266d098a8b7d9c9a3d861f1
JenBrainnet <benebrak01@gmail.com>
2026-03-15 15:51:27 +0200

    2. Tracker SQL [#1734] fix checkstyle error

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -60,7 +60,8 @@ public class SqlTracker implements Store {
     public boolean replace(int id, Item item) {
         boolean result;
         try (PreparedStatement statement =
-                     connection.prepareStatement("UPDATE items SET name = ?, created = ? WHERE id = ?")) {
+                     connection.prepareStatement(
+                             "UPDATE items SET name = ?, created = ? WHERE id = ?")) {
             statement.setString(1, item.getName());
             statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             statement.setInt(3, id);
